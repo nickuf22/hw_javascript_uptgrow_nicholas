f695589b01ac40c3dd6b6c2ac0c6a15afb07af71
Nicholas Uptgrow <nicholasuptgrow@gmail.com>
2017-06-09 23:51:13 -0400

    Add presidents to main js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var presidents = [
+	{
+		firstName: 'Donald',
+		lastName: 'Trump',
+		termLength: '1',
+		party: 'Republican',
+		yearsOfPresidency: '1'
+	};
+	{
+		firstName: 'Barack',
+		lastName: 'Obama',
+		termLength: '2',
+		party: 'Democratic',
+		yearsOfPresidency: '8'
+	};
+	{
+		firstName: 'George W.',
+		lastName: 'Bush',
+		termLength: '2',
+		party: 'Republican',
+		yearsOfPresidency: '8'
+	};
+	{
+		firstName: 'Bill',
+		lastName: 'Clinton',
+		termLength: '2',
+		party: 'Democratic',
+		yearsOfPresidency: '8'
+	};
+	{
+		firstName: 'George H. W.',
+		lastName: 'Bush',
+		termLength: '1',
+		party: 'Republican',
+		yearsOfPresidency: '4'
+	};
+];
+
+presidents[2].firstName.lastName;
