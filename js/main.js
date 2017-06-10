fe6c7076153a8da866fca1457344f8d42776ddd4
Nicholas Uptgrow <nicholasuptgrow@gmail.com>
2017-06-10 01:12:15 -0400

    Add numba

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ function howdy() {
 
 howdy();
 console.log( howdy() );
+
+
+var numba = function('string') {
+	if (string.length < 7)
+		return 'What a short little word!';
+	if (string.length > 7)
+		return 'I’m sorry, but that’s too many to count.';
+	if (string.length = 7)
+			return '7, what a perfect choice!'
+}
+console.log(numba(7))
+console.log(numba(seventy))
+console.log(numba(university))
